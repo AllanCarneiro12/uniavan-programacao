3f4a1ba6dfdc6d864f6a9d81d543bca1df04c8d9
Allan <allancarneirodacunha@gmail.com>
2025-10-07 21:38:18 -0300

    aula 07/10

---
+++ ExemploModolarizacao1/src/main/java/carneiro/exemplomodolarizacao1/ExemploModolarizacao1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package carneiro.exemplomodolarizacao1;
+
+/**
+ *
+ * @author Samsung
+ */
+public class ExemploModolarizacao1 {
+    
+    public static void entrada() {
+        System.out.println("\n\neu sou a funcao entrada");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        entrada();
+        processamento();
+        saida();
+    }
+}
